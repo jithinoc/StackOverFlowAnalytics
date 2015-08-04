e6edd169c4e8dc1fb5967c2ea4e89750379c9a48
jithinoc <jithino@qburst.com>
2015-08-04 15:46:46 +0530

    Add XMLEntityExtractor

--- src/main/java/com/qburst/stackOverFlowAnalytics/helpers/XMLEntityExtractor.java
+++ src/main/java/com/qburst/stackOverFlowAnalytics/helpers/XMLEntityExtractor.java
@@ -9,33 +9,19 @@ import java.io.PrintWriter;
 import java.util.HashSet;
 
 public class XMLEntityExtractor {
-    public void extract_attribute(String xmlPath, String writeTo) {
+    public void extract_attribute(String xmlPath, String writeTo) throws IOException{
         HashSet<String> locations = new HashSet<String >();
-        try (BufferedReader br = new BufferedReader(new FileReader(xmlPath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                line = line.trim().replaceFirst("^([\\W]+)<","<");
-                User user = (User)XMLObjectBuilder.xmlToObject(line, User.class);
-                System.out.println(user.getAccountId());
-                if(user.getLocation()!=null)
-                    locations.add(user.getLocation().toLowerCase());
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        BufferedReader xmlFileReader = new BufferedReader(new FileReader(xmlPath));
+        String line;
+        while ((line = xmlFileReader.readLine()) != null) {
+            line = line.trim().replaceFirst("^([\\W]+)<","<");
+            User user = (User)XMLObjectBuilder.xmlToObject(line, User.class);
+            if(user.getLocation()!=null)
+                locations.add(user.getLocation().toLowerCase());
         }
-        try {
-            PrintWriter writer = new PrintWriter(writeTo, "UTF-16");
-            for(String line: locations) {
-                writer.println(line);
-            }
-            writer.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        XMLEntityExtractor xmlEntityExtractor = new XMLEntityExtractor();
-        xmlEntityExtractor.extract_attribute("/home/jithinoc/Fatboy/stackoverflow/Users.xml", "/home/jithinoc/Fatboy/stackoverflow/locations.txt");
+        PrintWriter writeToWriter = new PrintWriter(writeTo, "UTF-16");
+        for(String location: locations)
+            writeToWriter.println(location);
+        writeToWriter.close();
     }
 }
